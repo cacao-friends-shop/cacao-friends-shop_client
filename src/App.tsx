2b7f318ac8c478ce887b8cec09ada652c7c67a16
jiwon <iamkjw77@gmail.com>
2021-04-26 16:11:09 +0900

    [Style] Global Style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import './App.css';
+import { Global } from '@emotion/react';
+import GlobalStyles from 'styles/GlobalStyles';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Global styles={GlobalStyles} />
+      <div className="App"></div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,161 @@
+import { css } from '@emotion/react';
+import { colors } from 'theme';
+
+const GlobalStyles = css`
+  html {
+    font-size: 10px;
+    overflow-y: visible;
+  }
+
+  body,
+  body *,
+  body::before,
+  body::after,
+  body *::before,
+  body *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+    font-size: 1.6rem;
+    font-weight: 400;
+    background-color: #fff;
+    color: ${colors.black};
+  }
+
+  a img {
+    vertical-align: middle;
+  }
+
+  fieldset {
+    border: 0;
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+  }
+
+  button[disabled] {
+    cursor: not-allowed;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+`;
+
+export default GlobalStyles;
