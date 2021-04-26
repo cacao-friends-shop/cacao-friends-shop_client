479b82248eb1d6aaa33e444bb0b7d99a1b77acab
yg-0103 <rladusrn89@gmail.com>
2021-04-26 20:01:57 +0900

    [Feat] IconButton 컴포넌트제작

---
+++ src/components/molecules/IconButton/IconButton.stories.tsx
@@ -0,0 +1,67 @@
+import IconButton from './';
+import { css } from '@emotion/css';
+
+export default {
+  component: IconButton,
+  title: 'components/IconButton',
+};
+
+export const homeIconButton = () => (
+  <IconButton name="home" size={40} fgColor="green" />
+);
+
+export const fillHeartIconButton = () => (
+  <IconButton name="fillHeart" size={40} fgColor="red" />
+);
+
+export const outlineHeartIconButton = () => (
+  <IconButton name="outlineHeart" size={40} fgColor="gray" />
+);
+
+const iconButtonTypes = [
+  'search',
+  'trash',
+  'close',
+  'fillHeart',
+  'outlineHeart',
+  'bubble',
+  'home',
+  'basket',
+  'pencil',
+  'menu',
+  'arrowLeft',
+  'arrowRight',
+  'arrowUp',
+  'arrowDown',
+  'circleArrowUp',
+];
+
+export const listOfIconButtons = () => {
+  return (
+    <ul className={iconButtonListStyle}>
+      {iconButtonTypes.map(icon => (
+        <li key={icon}>
+          <IconButton name={icon} size={20} fgColor="red" />
+
+          {icon}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const iconButtonListStyle = css`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  li {
+    box-sizing: border-box;
+    width: 25%;
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    svg {
+      margin-right: 1rem;
+    }
+  }
+`;
--- src/components/molecules/IconButton/index.tsx
+++ src/components/molecules/IconButton/index.tsx
@@ -3,18 +3,30 @@ import Icon from 'components/atoms/Icon';
 
 type IconButtonProps = {
   name: string;
-};
+  size: number;
+  bgColor?: string;
+  fgColor?: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const IconButton = ({ name }: IconButtonProps) => {
+const IconButton = ({
+  name,
+  size,
+  bgColor = 'none',
+  fgColor = 'inherit',
+  ...restProps
+}: IconButtonProps) => {
   return (
-    <button css={buttonStyle}>
-      <Icon name={name} />
+    <button css={buttonStyle(fgColor, bgColor)} {...restProps}>
+      <Icon name={name} size={size} />
     </button>
   );
 };
 
-const buttonStyle = css`
-  background: red;
+const buttonStyle = (fgColor: string, bgColor: string) => css`
+  background: ${bgColor};
+  color: ${fgColor};
+  padding: 0;
+  outline: none;
 `;
 
 export default IconButton;
