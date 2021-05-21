248fe9916a6cb3985ad0d2185d7c0cfd4eca1a90
iamkjw77 <iamkjw77@naver.com>
2021-05-21 21:50:30 +0900

    [Feat] 어드민 컴포넌트 수정

--- src/components/atoms/PostTitle/PostTitle.stories.tsx
+++ src/components/atoms/PostTitle/PostTitle.stories.tsx
@@ -1,3 +1,5 @@
+import { ContentType } from 'pages/AdminEdit/template';
+import { useState } from 'react';
 import PostTitle from '.';
 
 export default {
@@ -6,4 +8,16 @@ export default {
 };
 
 // 기본 포스트 타이틀
-export const postTitle = () => <PostTitle />;
+export const PostTitleBlock = () => {
+  const [value, setValue] = useState('');
+  const [, setContent] = useState<ContentType>({
+    title: '',
+    content: '',
+    characterType: '',
+    createdDateTime: '',
+    imageUrls: [],
+  });
+  return (
+    <PostTitle value={value} setValue={setValue} setContent={setContent} />
+  );
+};
